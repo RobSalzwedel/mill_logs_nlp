e0bfd8df255f5072da28ffff82af27812804ab2d
RobSalzwedel <rob.salzwedel@gmail.com>
2016-04-13 11:18:14 +0200

    STYLE: pep8 formatting

--- clean_logs.py
+++ clean_logs.py
@@ -12,25 +12,23 @@ Description: Plan to read the mill logs from CSV to pd.dataframe and clean up da
 # Have the raw data, but it is duplicated under different headings
 
 import pandas as pd
-import numpy as np
-import matplotlib.pyplot as plt
-import math
 
 #Read data
-data = pd.read_csv('mill_logs_2015.csv')
+DATA = pd.read_csv('mill_logs_2015.csv')
 
 # Step 1. Remove NaN's i
-def delete_nan(data ,column=''):
+def delete_nan(data, column=''):
     """Removes all the rows from data where a NaN appears in the specified column """
+
     for i in data.index:
-        if type(data[column][i]) == float:
+        if isinstance(data[column][i], float):
             data = data.drop([i])
     #Reset indices after dropping rows
-    data = data.reset_index().ix[:,1:]
+    data = data.reset_index().ix[:, 1:]
+
     return data
 
-
-def find_index(data,unit,column):
+def find_index(data, unit, column):
     """Finds the row where the specified unit appears in the data"""
     unit_index = data[column][data[column] == unit].index[0]
     return unit_index
@@ -44,41 +42,41 @@ def clean_data(data):
     #Step 1: Move additional information to the correct columns.
     #Additional information incorrectly appears in the 'date' column in raw_data.csv
     for i in data.index:
-        if  data['date'][i]=='Additional Information':
-            data['type'][i]= 'Additional Information'
+        if  data['date'][i] == 'Additional Information':
+            data['type'][i] = 'Additional Information'
             data['date'][i] = data['date'][i-1]
             data['description'][i] = data['date'][i+1]
 
     #Step 2: Find all the units in the date column and create a list
     units = []
     for unit in data['date']:
-        if type(unit) == str and len(unit) < 8:
+        if isinstance(unit, str) and len(unit) < 8:
             units.append(unit)
 
     #Step 3: Create a new column with the unit responsible for each log.
-    #Locate the index of each units mill logs in, it incorrectly appears in the 'date' column
+    #Locate the index of each units mill logs in, it incorrectly appears in the 'date' 
     unit_index = {}
     for unit in units:
-        unit_index[unit] = find_index(data,unit,'date')
+        unit_index[unit] = find_index(data, unit, 'date')
 
     #Create new column called unit
     data['unit'] = ''
 
     #Assign the correct unit based on the index located in the date columns
-    for i in range(0,len(units)-1):
+    for i in range(0, len(units)-1):
         data['unit'][unit_index[units[i]]:unit_index[units[i+1]]] = units[i]
     data['unit'][0:unit_index[units[0]]] = 'all'
     data['unit'][unit_index[units[-1]]:] = 'OP' # Need to clean this up (hacked)
 
     #Step 3. Remove all of the NaN's from the data
-    data = delete_nan(data,'type')
-    data = delete_nan(data,'description')
-    data = delete_nan(data,'date')
+    data = delete_nan(data, 'type')
+    data = delete_nan(data, 'description')
+    data = delete_nan(data, 'date')
 
     # Final index of each unit once all the NaNs are removed
     unit_index = {}
     for unit in units:
-        unit_index[unit] = find_index(data,unit,'unit')
+        unit_index[unit] = find_index(data, unit, 'unit')
 
     return data, unit_index
 
